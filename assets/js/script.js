221eacb4852d4325c99207058d7cf2753fd59f56
Mike Johnson <mike@mapleprintservices.com>
2022-10-03 23:39:22 -0400

    started basic structure, styles and pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// init
+// set score - define variable and set in localStorage
+var score = 0;
+
+// get score - retrieve from localStorage and update variable, display to page
+// high scores - array of objects containing score and initials
+// sort high scores array - high to low score
+// loop to create elements for each score and append to scores page
+// do we limit how many high scores? if so, add new score, sort, remove lowest after x 
+// start game
+// array of objects containing questions and answers, set order to a random order
+// loop through array and create question and answer HTML elements
+// append [0] to question section - question and options
+// answers will have data attributes of correct or incorrect
+// select an answer 
+// if correct, go to next question
+// if incorrect, go to next question, display wrong message with correct question and answer above in text reduce time by 15 seconds
+
+// start game
+// section#intro display: none
+// section#questions display: block
+// set timer to 59
+var setTime = function(){
+  var timeLeft = document.querySelector('#time-left')
+  timeLeft.textContent = 50;
+}
+setTime();
+// setInterval() {x, 1000}
